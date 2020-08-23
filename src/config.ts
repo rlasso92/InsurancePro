2b9a669f0c76b99da1db70c7b288ba24daed55bc
AT <>
2020-08-23 20:19:40 -0300

    add strict logLevel

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
+import { LogLevelDesc } from 'loglevel'
 dotenv.config()
 
 export const __PRODUCTION__ = process.env.NODE_ENV === 'production'
-export const __LOG_LEVEL__ = __PRODUCTION__ ? 'SILENT' : 'DEBUG'
+export const __LOG_LEVEL__: LogLevelDesc = __PRODUCTION__ ? 'SILENT' : 'DEBUG'
