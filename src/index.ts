961f99676fbee71846bf968e931a4cafdb19b9e9
AT <>
2020-08-23 19:49:46 -0300

    eslint/ts-parser + prettier

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import log from 'loglevel'
-import { __LOG_LEVEL__ } from './config';
+import { __LOG_LEVEL__ } from './config'
 
 log.setLevel(__LOG_LEVEL__)
 
 function greet(name: string) {
-  log.debug(`>>> Function 'greet' is called with ${name}`);
+  log.debug(`>>> Function 'greet' is called with ${name}`)
   console.log(`Hello, ${name}!`)
 }
 
