64707678228cecbe71a4134c1565831858d2c0be
AT <>
2020-08-30 19:47:47 -0300

    improve dev script

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv'
-import { LogLevelDesc } from 'loglevel'
+import log, { LogLevelDesc } from 'loglevel'
 dotenv.config()
 
 export const __PRODUCTION__ = process.env.NODE_ENV === 'production'
 export const __LOG_LEVEL__: LogLevelDesc = __PRODUCTION__ ? 'SILENT' : 'DEBUG'
+
+log.setLevel(__LOG_LEVEL__)
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
+import './config'
 import log from 'loglevel'
-import { __LOG_LEVEL__ } from './config'
-
-log.setLevel(__LOG_LEVEL__)
 
 function greet(name: string) {
   log.debug(`>>> Function 'greet' is called with ${name}`)
